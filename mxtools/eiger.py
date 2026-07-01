6fdc2a5846a3cf170cb0c9a55d1e48ca683bb512
vshekar1 <vshekar1@bnl.gov>
2026-07-01 11:39:37 -0400

    Move documents and datum to eiger detector, not flyer objects

--- mxtools/eiger.py
+++ mxtools/eiger.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 import os
 import time as ttime
+import uuid
 from collections import OrderedDict, deque
 from pathlib import PurePath
 from types import SimpleNamespace
@@ -167,64 +168,48 @@ class EigerSingleTriggerV26(SingleTrigger, EigerBaseV26):
     #         yield item
 
     def collect_asset_docs(self):
-        asset_docs_cache = []
+        ((_, image_resource),) = self.file.collect_asset_docs()
 
-        # Get the Resource which was produced when the detector was staged.
-        ((name, resource),) = self.file.collect_asset_docs()
+        root = image_resource["root"]
+        resource_path = image_resource["resource_path"]
+        seq_id = int(self.cam.sequence_id.get())
 
-        asset_docs_cache.append(("resource", resource))
-        self._datum_ids = DEFAULT_DATUM_DICT
-        # Generate Datum documents from scratch here, because the detector was
-        # triggered externally by the DeltaTau, never by ophyd.
-        resource_uid = resource["uid"]
-        # num_points = int(math.ceil(self.detector.cam.num_images.get() /
-        #                 self.detector.cam.fw_num_images_per_file.get()))
-
-        # We are currently generating only one datum document for all frames, that's why
-        #   we use the 0th index below.
-        #
-        # Uncomment & update the line below if more datum documents are needed:
-        # for i in range(num_points):
-
-        seq_id = self.cam.sequence_id.get()
-
-        self._master_file = f"{resource['root']}/{resource['resource_path']}_{seq_id}_master.h5"
+        self._master_file = f"{root}/{resource_path}_{seq_id}_master.h5"
         if not os.path.isfile(self._master_file):
             raise RuntimeError(f"File {self._master_file} does not exist")
 
-        # The pseudocode below is from Tom Caswell explaining the relationship between resource, datum, and events.
-        #
-        # resource = {
-        #     "resource_id": "RES",
-        #     "resource_kwargs": {},  # this goes to __init__
-        #     "spec": "AD-EIGER-MX",
-        #     ...: ...,
-        # }
-        # datum = {
-        #     "datum_id": "a",
-        #     "datum_kwargs": {"data_key": "data"},  # this goes to __call__
-        #     "resource": "RES",
-        #     ...: ...,
-        # }
-        # datum = {
-        #     "datum_id": "b",
-        #     "datum_kwargs": {"data_key": "omega"},
-        #     "resource": "RES",
-        #     ...: ...,
-        # }
+        resource_uid_image = image_resource["uid"]          # reuse existing uid
+        resource_uid_omega = str(uuid.uuid4())              # fresh uid for omega
 
-        # event = {...: ..., "data": {"detector_img": "a", "omega": "b"}}
+        image_resource_doc = {
+            "uid": resource_uid_image,
+            "spec": "AD_EIGER_MX",
+            "root": root,
+            "resource_path": resource_path,
+            "resource_kwargs": {"seq_id": seq_id, "dataset": "entry/data"},
+            "path_semantics": "posix",
+        }
+        omega_resource_doc = {
+            "uid": resource_uid_omega,
+            "spec": "AD_EIGER_MX_OMEGA",
+            "root": root,
+            "resource_path": resource_path,
+            "resource_kwargs": {"seq_id": seq_id, "dataset": "entry/sample/goniometer/omega"},
+            "path_semantics": "posix",
+        }
 
-        for data_key in self._datum_ids.keys():
-            datum_id = f"{resource_uid}/{data_key}"
-            self._datum_ids[data_key] = datum_id
-            datum = {
-                "resource": resource_uid,
-                "datum_id": datum_id,
-                "datum_kwargs": {"data_key": data_key},
-            }
-            asset_docs_cache.append(("datum", datum))
-        return tuple(asset_docs_cache)
+        datum_id_image = f"{resource_uid_image}/data"
+        datum_id_omega = f"{resource_uid_omega}/omega"
+
+        self._datum_ids["data"] = datum_id_image
+        self._datum_ids["omega"] = datum_id_omega
+
+        return (
+            ("resource", image_resource_doc),
+            ("resource", omega_resource_doc),
+            ("datum", {"resource": resource_uid_image, "datum_id": datum_id_image, "datum_kwargs": {"data_key": "data"}}),
+            ("datum", {"resource": resource_uid_omega, "datum_id": datum_id_omega, "datum_kwargs": {"data_key": "omega"}}),
+        )
 
     def _extract_metadata(self, field="omega"):
         with h5py.File(self._master_file, "r") as hf:
--- mxtools/flyer.py
+++ mxtools/flyer.py
@@ -94,64 +94,9 @@ class MXFlyer:
         }
 
     def collect_asset_docs(self):
-        asset_docs_cache = []
-
-        # Get the Resource which was produced when the detector was staged.
-        ((name, resource),) = self.detector.file.collect_asset_docs()
-
-        asset_docs_cache.append(("resource", resource))
-        self._datum_ids = DEFAULT_DATUM_DICT
-        # Generate Datum documents from scratch here, because the detector was
-        # triggered externally by the DeltaTau, never by ophyd.
-        resource_uid = resource["uid"]
-        # num_points = int(math.ceil(self.detector.cam.num_images.get() /
-        #                 self.detector.cam.fw_num_images_per_file.get()))
-
-        # We are currently generating only one datum document for all frames, that's why
-        #   we use the 0th index below.
-        #
-        # Uncomment & update the line below if more datum documents are needed:
-        # for i in range(num_points):
-
-        seq_id = self.detector.cam.sequence_id.get()
-
-        self._master_file = f"{resource['root']}/{resource['resource_path']}_{seq_id}_master.h5"
-        if not os.path.isfile(self._master_file):
-            raise RuntimeError(f"File {self._master_file} does not exist")
-
-        # The pseudocode below is from Tom Caswell explaining the relationship between resource, datum, and events.
-        #
-        # resource = {
-        #     "resource_id": "RES",
-        #     "resource_kwargs": {},  # this goes to __init__
-        #     "spec": "AD-EIGER-MX",
-        #     ...: ...,
-        # }
-        # datum = {
-        #     "datum_id": "a",
-        #     "datum_kwargs": {"data_key": "data"},  # this goes to __call__
-        #     "resource": "RES",
-        #     ...: ...,
-        # }
-        # datum = {
-        #     "datum_id": "b",
-        #     "datum_kwargs": {"data_key": "omega"},
-        #     "resource": "RES",
-        #     ...: ...,
-        # }
-
-        # event = {...: ..., "data": {"detector_img": "a", "omega": "b"}}
-
-        for data_key in self._datum_ids.keys():
-            datum_id = f"{resource_uid}/{data_key}"
-            self._datum_ids[data_key] = datum_id
-            datum = {
-                "resource": resource_uid,
-                "datum_id": datum_id,
-                "datum_kwargs": {"data_key": data_key},
-            }
-            asset_docs_cache.append(("datum", datum))
-        return tuple(asset_docs_cache)
+        if self.detector:
+            return self.detector.collect_asset_docs()
+        return {}
 
     def _extract_metadata(self, field="omega"):
         with h5py.File(self._master_file, "r") as hf:
